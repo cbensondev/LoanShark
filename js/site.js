fdf440fe9a770f4f575461033da141f091cb2f05
cbenson11 <67032200+cbenson11@users.noreply.github.com>
2021-08-28 21:05:30 -0500

    functional update

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 //Controller
-function GetValue() {
+function getValue() {
 
     let loanObj = {};
 
@@ -7,14 +7,15 @@ function GetValue() {
     loanObj.loanTerm = parseInt(document.getElementById("loanTerm").value);
     loanObj.loanRate = parseFloat(document.getElementById("loanRate").value);
 
-    AmortizationGen(loanObj);
+    amortizationGen(loanObj);
+    displayResult(loanObj)
 
 }
 
 //Amortization Generator
-function AmortizationGen(loanObj) {
+function amortizationGen(loanObj) {
     
-    loanObj.monthlyPayment = loanObj.loanAmount * (loanObj.loanRate / 1200) / (1 - (1 + (loanObj.loanRate / 1200)) - loanObj.loanTerm);
+    loanObj.monthlyPayment = loanObj.loanAmount * (loanObj.loanRate / 1200) / (1 - Math.pow((1 + (loanObj.loanRate / 1200)),-loanObj.loanTerm));
     loanObj.totalPrincipal = 0;
     loanObj.totalInterest = 0;
     loanObj.amortizationTable = [];
@@ -41,8 +42,41 @@ function AmortizationGen(loanObj) {
 }
 
 //Display results
-function DisplayResults() {
+function displayResult(loanObj) {
 
+    document.getElementById("monthlyPaymentValue").innerText = formatCurrency(loanObj.monthlyPayment);
+    document.getElementById("totalPrincipalValue").innerText = formatCurrency(loanObj.totalPrincipal);
+    document.getElementById("totalInterestValue").innerText = formatCurrency(loanObj.totalInterest);
+    let totalCost = loanObj.totalPrincipal + loanObj.totalInterest;
+    document.getElementById("totalCostValue").innerText = formatCurrency(totalCost);
+    document.getElementById("totalCostValue").classList.add("bold");
+
+    document.getElementById("amorTable").innerHTML = "";
+    let resultsTable = document.getElementById("amorTable");
+    
+    loanObj.amortizationTable.forEach(record => {
+        let tableRow = resultsTable.insertRow(-1);
+        let cell1 = tableRow.insertCell(0);
+        cell1.innerHTML = record.currentMonth;
+        let cell2 = tableRow.insertCell(1);
+        cell2.innerHTML = formatCurrency(loanObj.monthlyPayment);
+        let cell3 = tableRow.insertCell(2);
+        cell3.innerHTML = formatCurrency(record.principalPayment);
+        let cell4 = tableRow.insertCell(3);
+        cell4.innerHTML = formatCurrency(record.interestPayment);
+        let cell5 = tableRow.insertCell(4);
+        cell5.innerHTML = formatCurrency(record.totalInterest);
+        let cell6 = tableRow.insertCell(5);
+        cell6.innerHTML = formatCurrency(record.remainingBalance);
+    });
+
+}
+
+function formatCurrency(amount) {
+
+    let newAmount = amount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    newAmount = "$" + newAmount;
+    return newAmount;
 }
 
 
